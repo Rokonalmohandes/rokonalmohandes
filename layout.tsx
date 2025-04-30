4e009d4e6fe581f2105849109528393e0e2dc1fe
Rokonalmohandes <info.roknalmohandes@gmail.com>
2025-04-30 19:07:01 +0300

    Add files via upload
    اصلاح ايقونة الموقع

--- layout.tsx
+++ layout.tsx
@@ -21,7 +21,13 @@ export const metadata: Metadata = {
   title: "ركن المهندس",
   description: "خدمات أكاديمية واحترافية للطلبة والأساتذة",
   icons: {
-    icon: "the final kofi.png", // Path relative to public folder
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+    ],
   },
 };
 
